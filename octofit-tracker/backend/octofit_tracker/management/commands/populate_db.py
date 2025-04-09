98ebbfe656c0a62a92b0e8d2c29a105adf4ee437
Marianda <102419863+Marianda587@users.noreply.github.com>
2025-04-09 13:38:21 +0000

    Updated populate_db.py to test direct MongoDB queries and verify data persistence

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -2,6 +2,10 @@ from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
 from bson import ObjectId
 from datetime import timedelta
+from pymongo import MongoClient
+from django.conf import settings
+
+print('Command class loaded successfully')
 
 class Command(BaseCommand):
     help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
@@ -16,17 +20,21 @@ class Command(BaseCommand):
 
         # Create users
         users = [
-            User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='password'),
-            User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='password'),
-            User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='password'),
-            User(_id=ObjectId(), username='crashoverride', email='crashoverride@mhigh.edu', password='password'),
-            User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@mhigh.edu', password='password'),
+            User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='password123'),
+            User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='password123'),
+            User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='password123'),
+            User(_id=ObjectId(), username='crashoverride', email='crashoverride@mhigh.edu', password='password123'),
+            User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@mhigh.edu', password='password123'),
         ]
+        print('Creating users...')
+        print(users)
         User.objects.bulk_create(users)
 
         # Create teams
         team1 = Team(_id=ObjectId(), name='Blue Team')
         team2 = Team(_id=ObjectId(), name='Gold Team')
+        print('Creating teams...')
+        print([team1, team2])
         team1.save()
         team2.save()
         team1.members.add(users[0], users[1])
@@ -40,6 +48,8 @@ class Command(BaseCommand):
             Activity(_id=ObjectId(), user=users[3], activity_type='Strength', duration=timedelta(minutes=30)),
             Activity(_id=ObjectId(), user=users[4], activity_type='Swimming', duration=timedelta(hours=1, minutes=15)),
         ]
+        print('Creating activities...')
+        print(activities)
         Activity.objects.bulk_create(activities)
 
         # Create leaderboard entries
@@ -50,6 +60,8 @@ class Command(BaseCommand):
             Leaderboard(_id=ObjectId(), user=users[3], score=85),
             Leaderboard(_id=ObjectId(), user=users[4], score=80),
         ]
+        print('Creating leaderboard entries...')
+        print(leaderboard_entries)
         Leaderboard.objects.bulk_create(leaderboard_entries)
 
         # Create workouts
@@ -60,6 +72,19 @@ class Command(BaseCommand):
             Workout(_id=ObjectId(), name='Strength Training', description='Training for strength'),
             Workout(_id=ObjectId(), name='Swimming Training', description='Training for a swimming competition'),
         ]
+        print('Creating workouts...')
+        print(workouts)
         Workout.objects.bulk_create(workouts)
+
+        # Insert a test user
+        user = User(_id=ObjectId(), username='testuser', email='testuser@example.com', password='password123')
+        user.save()
+        print('Test user inserted:', user)
+
+        # Test direct MongoDB connection
+        client = MongoClient(settings.DATABASES['default']['HOST'], settings.DATABASES['default']['PORT'])
+        db = client[settings.DATABASES['default']['NAME']]
+        print('Collections in database:', db.list_collection_names())
+        print('Users collection data:', list(db.users.find()))
 
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -20,70 +20,70 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e0)b8+dgg%#r!*_492!n137sx#+c#-snz0g=acvt%9fq4ui*7q"
+SECRET_KEY = 'django-insecure-25rsll_s*6ml5lv4l$51z6x!y5u_k!11f!hf^1&%q!$syk=ja3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "rest_framework",
-    "djongo",
-    "corsheaders",
-    "octofit_tracker",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'djongo',
+    'corsheaders',
+    'octofit_tracker',
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
-ROOT_URLCONF = "octofit_tracker.urls"
+ROOT_URLCONF = 'octofit_tracker.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "octofit_tracker.wsgi.application"
+WSGI_APPLICATION = 'octofit_tracker.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "djongo",
-        "NAME": "octofit_db",
-        "HOST": "localhost",
-        "PORT": 27017,
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
+        'HOST': 'localhost',
+        'PORT': 27017,
     }
 }
 
@@ -93,16 +93,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -122,11 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
